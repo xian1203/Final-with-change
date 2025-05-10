f21a00c644636da46a8bc7b93bd36c72984437ef
gerger117 <programsub31@gmail.com>
2025-05-10 23:07:28 +0800

    Update SignIn.tsx

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -33,7 +33,7 @@ const SignIn = () => {
         <CardHeader>
           <div className="flex justify-center mb-4">
             <img
-              src="assets/logo.png" // Ensure this path matches the actual location of the logo
+              src={`${process.env.PUBLIC_URL}/assets/logo.png`}
               alt="Logo"
               className="h-16 w-16 object-contain"
             />
